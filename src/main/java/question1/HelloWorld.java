a202ae74cb06527d1cd2d359105f2248b8e5c32d
Abhishek <abhishekarora.official@gmail.com>
2018-02-20 15:26:06 +0530

    committing

---
+++ src/main/java/question1/HelloWorld.java
@@ -0,0 +1,12 @@
+package question1;
+
+import question2.HelloWorldAbhi;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        HelloWorldAbhi.main(new String[1]);
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/main/java2/question2/HelloWorldAbhi.java
@@ -0,0 +1,7 @@
+package question2;
+public class HelloWorldAbhi {
+    public static void main(String[] args) {
+        System.out.println("Hello World2");
+    }
+
+}
