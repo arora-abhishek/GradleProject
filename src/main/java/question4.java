d0041176e2e061953f8bacddb80745622cd5a36e
Abhishek <abhishekarora.official@gmail.com>
2018-02-20 16:06:16 +0530

    comitting

---
+++ src/main/java/question4.java
@@ -0,0 +1,12 @@
+import org.apache.commons.lang3.StringUtils;
+public class question4 {
+
+
+        public static void main(String[] args) {
+            String str="Abhishek";
+            String upr= StringUtils.upperCase(str);
+            System.out.println(upr);
+        }
+
+
+}
